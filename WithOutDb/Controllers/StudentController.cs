6b8f9cd0443f6242e9642d7f00152a5b9cf107d2
Radhesh-Roy <radheshroy0011@gmail.com>
2024-02-25 12:34:14 +0600

    CRUD without Database

---
+++ WithOutDb/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using WithOutDb.Models;
+
+namespace WithOutDb.Controllers
+{
+    public class StudentController: Controller
+    {
+       
+
+        List<Student> students = new List<Student>()
+            {
+            new Student(1,343,"Radhesh","CSE","Eight","rj@gmail.com"),
+            new Student(2,3434,"Joy","CSE","Eight","rj@gmail.com"),
+            new Student(3,34343,"Sofik","CSE","Eight","rj@gmail.com"),
+            new Student(4,3434,"Nipon","CSE","Eight","rj@gmail.com"),
+            new Student(5,3435,"Jibon","CSE","Eight","rj@gmail.com"),
+            new Student(6,553,"Radhesh","CSE","Eight","rj@gmail.com"),
+            };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult CreateOrEdit()
+        {
+            return View();  
+
+        }
+
+        [HttpPost]  
+        public IActionResult CreateOrEdit(int id,Student student)
+        {
+            if (id==0)
+            {
+                
+                    students.Add(student);
+                    return RedirectToAction(nameof(Index));
+                
+            }
+            else
+            {
+               
+                return RedirectToAction(nameof(Index));
+            }
+
+            
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int id,Student student)
+        {
+            var data = students[id];
+            if (data !=null)
+            {
+                students.Remove(student);
+                return RedirectToAction(nameof(Index));
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WithOutDb/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace WithOutDb.Models;
+
+public class Student
+{
+    public int Id { get; set; } 
+    public int Roll { get; set; } = default;
+    public string ?Name { get; set; } = default;
+    public string ?Department { get; set; } = default;
+    public string  ?Semister { get; set; } = default;
+    public string ?Email { get; set; } = default;
+
+    public Student(int Id,int Roll, string Name, string Department, string Semister, string Email)
+    {
+        this.Id = Id;
+        this.Roll = Roll;
+        this.Name = Name;
+        this.Department = Department;
+        this.Semister = Semister;
+        this.Email = Email;
+    }
+    public Student()
+    {
+        
+    }
+}
